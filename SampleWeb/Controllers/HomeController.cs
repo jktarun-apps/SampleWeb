0e8d94a96bb7e7d5a39ba3a8e78c018ba5ecfa2b
jktarun <jktarun@gmail.com>
2025-08-04 22:53:59 +0530

    Push all changes

--- SampleWeb/Controllers/HomeController.cs
+++ SampleWeb/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace SampleWeb.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult ErrorV1()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
